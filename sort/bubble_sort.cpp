e2906a0f91d46f88f3c8141efaf19973b4a67a2f
DuyNam <duynamk63uet@gmail.com>
2019-06-20 14:15:22 +0700

    Upload

--- sort/bubble_sort.cpp
+++ sort/bubble_sort.cpp
@@ -4,28 +4,30 @@ using namespace std;
 
 void bubbleSort(int arr[], int len)
 {
-	for(int i = 0; i < len - 1; i++)
-	{
-		for(int j = i; j < len; j++)
-		{
-			if(arr[j] < arr[i])
-			{
-				int temp = arr[i];
+    for(int i = 0; i < len - 1; i++)
+    {
+        for(int j = i; j < len; j++)
+        {
+            if(arr[j] < arr[i])
+            {
+                int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
-			}
-		}
-	}
-	return;
+            }
+        }
+    }
+    return;
 }
 
 int main()
 {
     int arr[10] = {2, 5, 1, 6, 9, 3, 0, 8, 7, 4};
+    
     bubbleSort(arr, 10);
+    
     for(int i = 0; i < 10; i++)
     {  
         cout << arr[i] << " ";
     }
-	return 0;
+    return 0;
 }
--- sort/insertion_sort.cpp
+++ sort/insertion_sort.cpp
@@ -4,40 +4,30 @@ using namespace std;
 
 void insertion_sort(int arr[], int len)
 {
-	for(int i = 1; i < len; i++)
-	{
-		int key = arr[i];
-		//insert arr[i] into the sorted sequence arr[0 .. i - 1]
-		int j = i - 1;
-		while(j >= 0 && arr[j] > key)
-		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = key;
-	}
-<<<<<<< HEAD
+    for(int i = 1; i < len; i++)
+    {
+        int key = arr[i];
+        //insert arr[i] into the sorted sequence arr[0 .. i - 1]
+        int j = i - 1;
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
     return;
-=======
-	return;
->>>>>>> 87849e2f625eb6a53364c0bab21d991b177bfc21
 }
 
 int main()
 {
 	int arr[10] = {2, 3, 1, 0, 9, 6, 8, 4, 5, 7};
-<<<<<<< HEAD
+
     insertion_sort(arr, 10);
-    for(int i = 0; i < 10; i++)
-    {
-        cout << arr[i] << " ";
-    }
-=======
-	insertion_sort(arr, 10);
+
 	for(int i = 0; i < 10; i++)
 	{
 		cout << arr[i] << " ";
 	}
->>>>>>> 87849e2f625eb6a53364c0bab21d991b177bfc21
 	return 0;
 }
